a9809fdb483023827778c349575d3ee844c5f0ab
wanghaozhen <haozhenwang297@gmail.com>
2024-08-26 20:59:43 +0800

    Add Enums to UserInputHandler
    Changed if else logic to use enums instead. Using enums allows the usage of switch case which improves readability and maintainability of the code base compared to if else statements. Moved printList() method to UserInputHandler as it made sense that UserInputHandler should provide this feature instead of Friday. Finally, moved more input cases into UserInputHandler.

--- src/main/java/Friday.java
+++ src/main/java/Friday.java
@@ -16,61 +16,18 @@ public class Friday {
         this.list = new ArrayList<>();
     }
 
-    public String printList() {
-        String items = "";
-        if (this.list.isEmpty()) {
-            return "\t  There are currently no items in your list.";
-        }
-        for (int i = 0; i < this.list.size(); i++) {
-            String d = String.format("\t  %s.%s", i + 1, this.list.get(i));
-            if (i == this.list.size() - 1) {
-                items = items.concat(d);
-            } else {
-                items = items.concat(d + "\n");
-            }
-        }
-        return items;
-    }
     public void start() {
         System.out.println(header);
         while (true) {
             try {
                 String input = this.handler.getInput();
-                if (input.equals("bye")) {
+                if (input.trim().equals("bye")) {
                     System.out.println(this.ending);
                     break;
                 }
-                if (input.equals("list")) {
-                    String description = "\t Here are the tasks in your list:" + "\n" + this.printList();
-                    BotMessage output = new BotMessage(description);
-                    System.out.println(output);
-                    continue;
-                }
-                if (input.startsWith("mark")) {
-                    int idx = Integer.parseInt(input.substring(5));
-                    this.list.get(idx - 1).markAsDone();
-                    String description = "\t Nice! I've marked this task as done:" + "\n" + this.list.get(idx - 1);
-                    BotMessage output = new BotMessage(description);
-                    System.out.println(output);
-                    continue;
-                }
-                if (input.startsWith("unmark")) {
-                    int idx = Integer.parseInt(input.substring(7));
-                    this.list.get(idx - 1).markAsUndone();
-                    String description = "\t OK, I've marked this task as not done yet:" + "\n" + this.list.get(idx - 1);
-                    BotMessage output = new BotMessage(description);
-                    System.out.println(output);
-                    continue;
-                }
-
-                // Any other input
                 String response = this.handler.handleInput(input, list);
                 BotMessage output = new BotMessage(response);
                 System.out.println(output);
-
-            } catch (DescriptionException e) {
-                BotMessage message = new BotMessage(e.getMessage());
-                System.out.println(message);
             } catch (FridayException e) {
                 BotMessage message = new BotMessage(e.getMessage());
                 System.out.println(message);
--- src/main/java/UserInputHandler.java
+++ src/main/java/UserInputHandler.java
@@ -3,106 +3,148 @@ import java.util.*;
 public class UserInputHandler {
 
     private final Scanner scanner;
-    private final Set<String> validCommands = new HashSet<>(Arrays.asList("bye", "list", "mark", "unmark", "todo", "deadline", "event", "delete"));
-    private final String ending = """
-             \t  Bye. Hope to see you again soon!""";
+    private enum Command {
+        LIST, MARK, UNMARK, TODO, DEADLINE, EVENT, DELETE, UNKNOWN;
 
+        public static Command fromString(String input) {
+            try {
+                return Command.valueOf(input.toUpperCase());
+            } catch (IllegalArgumentException e) {
+                return UNKNOWN;
+            }
+        }
+    }
     public UserInputHandler() {
         this.scanner = new Scanner(System.in);
     }
 
     public String getInput() throws InputException {
-        String input = this.scanner.nextLine();
-        for (String command : validCommands) {
-            if (input.startsWith(command)) {
-                return input;
+        return this.scanner.nextLine();
+    }
+
+    public String printList(ArrayList<Task> list) {
+        String items = "";
+        if (list.isEmpty()) {
+            return "\t  There are currently no items in your list.";
+        }
+        for (int i = 0; i < list.size(); i++) {
+            String d = String.format("\t  %s.%s", i + 1, list.get(i));
+            if (i == list.size() - 1) {
+                items = items.concat(d);
+            } else {
+                items = items.concat(d + "\n");
             }
         }
-        throw new InputException("Sorry, I have honestly 0 clue what you are saying... T.T");
+        return items;
     }
 
     public String handleInput(String input, ArrayList<Task> list)  throws FridayException {
 
-        Task task = null;
+        String[] parts = input.trim().split(" ", 2);
+        Command command = Command.fromString(parts[0]);
+        int idx;
+        Task task;
 
-        //Logic for Todo task
-        if (input.startsWith("todo")) {
-            if (input.trim().length() == 4 || input.substring(5).trim().isEmpty()) {
-                throw new DescriptionException("todo");
-            }
-            task = new Todo(input.substring(5).trim());
-            list.add(task);
+        switch (command) {
+
+            case LIST:
+                return "\t Here are the tasks in your list:" + "\n" + this.printList(list);
+
+            case MARK:
+                idx = Integer.parseInt(input.substring(5).trim());
+                list.get(idx - 1).markAsDone();
+                return "\t Nice! I've marked this task as done:" + "\n" + "\t  " + list.get(idx - 1);
+
+            case UNMARK:
+                idx = Integer.parseInt(input.substring(7).trim());
+                list.get(idx - 1).markAsUndone();
+                return "\t OK, I've marked this task as not done yet:" + "\n" + "\t  " + list.get(idx - 1);
+
+            case TODO:
+                    if (input.trim().length() == 4 || input.substring(5).trim().isEmpty()) {
+                        throw new DescriptionException("todo");
+                    }
+                    task = new Todo(input.substring(5).trim());
+                    list.add(task);
+                    return String.format(""" 
+                        \t Got it. I've added this task:
+                        \t  %s
+                        \t Now you have %d tasks in the list.""", task, list.size());
+
+            //Logic for Deadline task
+            case DEADLINE:
+                int end = input.indexOf("/by");
+                if (!input.contains("/by")) {
+                    throw new MissingCommandException("/by");
+                }
+                String deadlineDescription = input.substring(input.indexOf("deadline") + "deadline".length(), end).trim();
+                if (deadlineDescription.isEmpty()) {
+                    throw new DescriptionException("deadline");
+                }
+                String deadline = input.substring(end + 3).trim();
+                if (deadline.isEmpty()) {
+                    throw new DeadlineException("deadline");
+                }
+                task = new Deadline(deadlineDescription, deadline);
+                list.add(task);
+                return String.format(""" 
+                    \t Got it. I've added this task:
+                    \t  %s
+                    \t Now you have %d tasks in the list.""", task, list.size());
+
+
+            //Logic for Event task
+            case EVENT:
+                if (!input.contains("/from")) {
+                    throw new MissingCommandException("/from");
+                }
+                if (!input.contains("/to")) {
+                    throw new MissingCommandException("/to");
+                }
+                int eventIndex = input.indexOf("event") + "event".length();
+                int fromIndex = input.indexOf("/from");
+                int toIndex = input.indexOf("/to");
+                String eventDescription = input.substring(eventIndex, fromIndex).trim();
+                if (eventDescription.isEmpty()) {
+                    throw new DescriptionException("event");
+                }
+                String from = input.substring(fromIndex + "/from".length(), toIndex).trim();
+                if (from.isEmpty()) {
+                    throw new FromException();
+                }
+                String to = input.substring(toIndex + "/to".length()).trim();
+                if (to.isEmpty()) {
+                    throw new ToException();
+                }
+                task = new Event(eventDescription, from, to);
+                list.add(task);
+                return String.format(""" 
+                    \t Got it. I've added this task:
+                    \t  %s
+                    \t Now you have %d tasks in the list.""", task, list.size());
+
+            //Logic for Delete command
+            case DELETE:
+                String i = input.substring("delete".length()).trim();
+                if (i.isEmpty()) {
+                    throw new InputException("Please give a valid item for me to delete. Try again.");
+                }
+                idx = Integer.parseInt(i) - 1;
+                if (list.isEmpty()) {
+                    throw new FridayException("Attempting to delete item from an empty list. Please add tasks first.");
+                }
+                if (idx >= list.size() || idx < 0) {
+                    throw new FridayException("Attempting to delete item which is not in the list. Please ensure the number is correct.");
+                }
+                Task r = list.remove(idx);
+                return String.format(""" 
+                        \t Got it. I've removed this task:
+                        \t  %s
+                        \t Now you have %d tasks in the list.""", r, list.size());
+
+            case UNKNOWN:
+            default:
+                throw new InputException("Sorry, I have honestly 0 clue what you are saying... T.T");
         }
-
-        //Logic for Deadline task
-        if (input.startsWith("deadline")) {
-            int end = input.indexOf("/by");
-            if (!input.contains("/by")) {
-                throw new MissingCommandException("/by");
-            }
-            String description = input.substring(input.indexOf("deadline") + "deadline".length(), end).trim();
-            if (description.isEmpty()) {
-                throw new DescriptionException("deadline");
-            }
-            String deadline = input.substring(end + 3).trim();
-            if (deadline.isEmpty()) {
-                throw new DeadlineException("deadline");
-            }
-            task = new Deadline(description, deadline);
-            list.add(task);
-        }
-
-        //Logic for Event task
-        if (input.startsWith("event")) {
-            if (!input.contains("/from")) {
-                throw new MissingCommandException("/from");
-            }
-            if (!input.contains("/to")) {
-                throw new MissingCommandException("/to");
-            }
-            int eventIndex = input.indexOf("event") + "event".length();
-            int fromIndex = input.indexOf("/from");
-            int toIndex = input.indexOf("/to");
-            String description = input.substring(eventIndex, fromIndex).trim();
-            if (description.isEmpty()) {
-                throw new DescriptionException("event");
-            }
-            String from = input.substring(fromIndex + "/from".length(), toIndex).trim();
-            if (from.isEmpty()) {
-                throw new FromException();
-            }
-            String to = input.substring(toIndex + "/to".length()).trim();
-            if (to.isEmpty()) {
-                throw new ToException();
-            }
-            task = new Event(description, from, to);
-            list.add(task);
-        }
-
-        //Logic for Delete command
-        if (input.startsWith("delete")) {
-            String i = input.substring("delete".length()).trim();
-            if (i.isEmpty()) {
-                throw new InputException("Please give a valid item for me to delete. Try again.");
-            }
-            int idx = Integer.parseInt(i) - 1;
-            if (list.isEmpty()) {
-                throw new FridayException("Attempting to delete item from an empty list. Please add tasks first.");
-            }
-            if (idx >= list.size() || idx < 0) {
-                throw new FridayException("Attempting to delete item which is not in the list. Please ensure the number is correct.");
-            }
-            Task r = list.remove(idx);
-            return String.format(""" 
-            \t Got it. I've removed this task:
-            \t  %s
-            \t Now you have %d tasks in the list.""", r, list.size());
-        }
-
-        return String.format(""" 
-            \t Got it. I've added this task:
-            \t  %s
-            \t Now you have %d tasks in the list.""", task, list.size());
     }
-
 }
