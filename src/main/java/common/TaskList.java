b88b2cde776696adfe43dfcf4d4bb01243f4b517
wanghaozhen <haozhenwang297@gmail.com>
2024-09-12 10:44:32 +0800

    Add Stream logic to TaskList
    Changed findTasks to implement java Streams.

--- src/main/java/common/TaskList.java
+++ src/main/java/common/TaskList.java
@@ -1,12 +1,14 @@
 package common;
 
+import java.sql.Array;
 import java.util.ArrayList;
+import java.util.stream.Collectors;
 
 import tasks.Task;
 
 
 public class TaskList {
-    private final ArrayList<Task> list = new ArrayList<>();
+    private ArrayList<Task> list = new ArrayList<>();
 
     public void add(Task task) {
         this.list.add(task);
@@ -18,6 +20,10 @@ public class TaskList {
 
     public ArrayList<Task> getList() {
         return this.list;
+    }
+
+    public void setList(ArrayList<Task> list) {
+        this.list = list;
     }
 
     public boolean isEmpty() {
@@ -33,12 +39,11 @@ public class TaskList {
     }
 
     public TaskList findTasks(String keyword) {
-        TaskList filtered = new TaskList();
-        for (Task task : this.list) {
-            if (task.getDescription().contains(keyword)) {
-                filtered.add(task);
-            }
-        }
-        return filtered;
+        TaskList filteredList = new TaskList();
+        ArrayList<Task> filtered = new ArrayList<>(this.list.stream()
+                                       .filter(task -> task.getDescription()
+                                       .contains(keyword)).collect(Collectors.toList()));
+        filteredList.setList(filtered);
+        return filteredList;
     }
 }
